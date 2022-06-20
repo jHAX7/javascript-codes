54d908893710f7d5fa57d516a0aba26124decd4c
BlooketHacks.au <107653282+jHAX7@users.noreply.github.com>
2022-06-21 07:45:53 +1000

    Create moving-dancing images.js

---
+++ .js scripts for inspect element/moving-dancing images.js
@@ -0,0 +1,14 @@
+javascript:
+ R=0; x1=.1; y1=.05; x2=.25; y2=.24; x3=1.6; y3=.24; x4=300; y4=200;  x5=300; y5=200;
+ DocumentImage=document.getElementsByTagName("img"); 
+ DocLength=DocumentImage.length;
+function DancingImage(){
+ for(i=0; i-DocLength; i++){
+    DIS=DocumentImage[i].style; 
+    DIS.position='absolute';
+    DIS.left=(Math.sin(R*x1+i*x2+x3)*x4+x5)+"px"; 
+    DIS.top=(Math.cos(R*y1+i*y2+y3)*y4+y5)+"px";
+ }
+ R++
+}
+setInterval('DancingImage()',5); void(0);
