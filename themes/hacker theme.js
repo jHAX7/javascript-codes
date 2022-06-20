e3680522c72c536c4e7b5265a1c5cb52c415d25e
BlooketHacks.au <107653282+jHAX7@users.noreply.github.com>
2022-06-21 07:37:48 +1000

    Create hacker theme.js

---
+++ themes/hacker theme.js
@@ -0,0 +1,6 @@
+var allDivElements = document.querySelectorAll('div');
+for(var i = 0; i < allDivElements.length; i++){
+  allDivElements[i].style['font-family'] = 'Gochi Hand';
+  allDivElements[i].style['background-color'] = 'black';
+  allDivElements[i].style['color'] = 'green';
+}
