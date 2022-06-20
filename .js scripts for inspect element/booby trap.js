305f745915ea75e56cb69292f0552b14ce86f0e2
BlooketHacks.au <107653282+jHAX7@users.noreply.github.com>
2022-06-21 07:51:16 +1000

    Create booby trap.js

---
+++ .js scripts for inspect element/booby trap.js
@@ -0,0 +1,8 @@
+
+setTimeout(function(){
+  document.onmousemove = document.onkeypress = function(){
+  ['', '-ms-', '-webkit-', '-o-', '-moz-'].map(function(prefix){
+    document.body.style[prefix + 'transition'] = prefix + 'transform 3s';
+    document.body.style[prefix + 'transform'] = 'rotate(180deg)';
+  });
+}}, 4000);
